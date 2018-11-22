d423497866d2e810968ac910968e1d7471c37a59
hiltonmarins <hiltonmarins@gmail.com>
2018-11-22 17:16:35 -0200

    Update Principal.java

--- temp/src/Principal.java
+++ temp/src/Principal.java
@@ -1,7 +1,7 @@
 
 public class Principal {
 	public static void main(String[] args) {
-		Produto p = new Produto("Café", 50, 7.5);
+		Produto p = new Produto("CafÃ©", 50, 7.5);
 		try {
 			Venda v = new Venda(p, -100);
 			System.out.println("Venda efetuada com sucesso.");
@@ -9,9 +9,7 @@ public class Principal {
 			e.printStackTrace();
 		}
 		System.out.println("fim.");
-		
-		
-				
 	}
+	//alterado no git
 
 }
